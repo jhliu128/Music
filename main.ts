5bf355b105537ccbcf48a6678635b9ec23556c15
jhliu128 <56717051+jhliu128@users.noreply.github.com>
2019-10-23 05:00:19 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace Music {
     * 演奏 感謝
     */
     //% blockId="thank" block="play thank"
-    //% blockGap=2 weight=2
+    //% blockGap=2 weight=0
     export function thank(): void {
     music.playTone(196, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Whole))
@@ -111,50 +111,12 @@ namespace Music {
     music.rest(music.beat(BeatFraction.Whole))
     }
 
-    /**
-    * 演奏 母親像月亮
-    */
-    //% blockId="moon" block="play moon"
-    //% blockGap=2 weight=0
-    export function moon(): void {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Half))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(196, music.beat(BeatFraction.Whole))
-    music.playTone(220, music.beat(BeatFraction.Double))
-    music.playTone(196, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(196, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(349, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(196, music.beat(BeatFraction.Half))
-    music.playTone(247, music.beat(BeatFraction.Whole))
-    music.playTone(196, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Double))
-    }
 
    /**
     * 演奏 魯冰花
     */
     //% blockId="lupinus" block="play lupinus"
-    //% blockGap=2 weight=3
+    //% blockGap=2 weight=1
     export function lupinus(): void {
     music.playTone(440, music.beat(BeatFraction.Whole))
     music.rest(music.beat(BeatFraction.Half))
@@ -220,13 +182,54 @@ namespace Music {
     }
 
 
+
+    /**
+    * 演奏 母親像月亮
+    */
+    //% blockId="moon" block="play moon"
+    //% blockGap=2 weight=2
+    export function moon(): void {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Double))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    }
+
+
  
 
     /**
     * 演奏 甜蜜的家庭
     */
     //% blockId="sweat_family" block="play sweat_family"
-    //% blockGap=2 weight=5
+    //% blockGap=2 weight=3
     export function sweat_family(): void {
     music.rest(music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Half))
